8019f56719409fdec72e73f74bbb9040766951ee
nguyen.dinh.thach <nguyendinhthach@sun-asterisk.com>
2020-12-22 16:59:24 +0700

    - Fix update lastest field

--- XData/FirebaseHelper.swift
+++ XData/FirebaseHelper.swift
@@ -13,37 +13,41 @@ class FirebaseHelper {
     let db = Firestore.firestore()
     
     func addRecord(data: [String : Any], completionHandler: @escaping (_ err: Error?) -> Void) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
         if let date = data["date"], date is String {
             db.collection("score").document(date as! String).setData(data) { err in
                 if let err = err {
                     completionHandler(err)
                 } else {
+                    self.lastestDate { err, lastestDate in
+                        if let checkDate = dateFormatter.date(from: date as! String), let _lastestDate = lastestDate, let lastest = dateFormatter.date(from: _lastestDate) {
+                            if Calendar.current.compare(checkDate, to: lastest, toGranularity: .day) == .orderedDescending {
+                                self.db.collection("info").document("lastestUpdate").setData(["date" : date]) { err in
+                                    //Do something
+                                }
+                            }
+                        }
+                    }
                     completionHandler(nil)
                 }
             }
         }
     }
+
     
-    func lastestDate(completionHandler: @escaping (_ err: Error?, _ date: String?) -> Void){
-        db.collection("score").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                completionHandler(err, nil)
-            } else {
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "dd-MM-yyyy"
-                if var lastestDate = dateFormatter.date(from: "01-01-2006") {
-                    for document in querySnapshot!.documents {
-                        if let documentDate = dateFormatter.date(from: document.documentID) {
-                            if Calendar.current.compare(documentDate, to: lastestDate, toGranularity: .day) == .orderedDescending {
-                                lastestDate = documentDate
-                            } else {
-                                continue
-                            }
-                        }
-                    }
-                    let dateString = dateFormatter.string(from: lastestDate)
-                    completionHandler(nil, dateString)
+    func lastestDate(completionHandler: @escaping (_ err: Error?, _ date: String?) -> Void) {
+        let infoRef = db.collection("info")
+        let lastestUpdate = infoRef.document("lastestUpdate")
+        lastestUpdate.getDocument { (document, err) in
+            if let document = document, document.exists {
+                if let data = document.data(), let date = data["date"]{
+                    completionHandler(nil, date as! String)
+                } else {
+                    completionHandler(nil, nil)
                 }
+            } else {
+                completionHandler(err, nil)
             }
         }
     }
